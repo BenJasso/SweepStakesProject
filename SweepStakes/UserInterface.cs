9450da2aab412912451c170ce7210b427a2dd599
Ben Jasso <benjaminjasso@icloud.com>
2020-04-06 13:51:50 -0500

    Initial commit with skeleton

---
+++ SweepStakes/UserInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    static class UserInterface
+    {
+
+        //member variables
+
+
+        //constructor
+
+
+        //member methods
+        public string GetUserInputFor(string prompt)
+        {
+
+        }
+    }
+}
