edebee44b4324d48cb98469a82cf10e4046e1ed4
Ben Jasso <benjaminjasso@icloud.com>
2020-04-08 11:06:27 -0500

    notify contestants complete

--- SweepStakes/Program.cs
+++ SweepStakes/Program.cs
@@ -11,45 +11,7 @@ namespace SweepStakes
         static void Main(string[] args)
         {
 
-            ISweepStakesManager sweepStakesManager = UserInterface.GetChoiceOfManager();
-            MarketingFirm marketingFirm = new MarketingFirm(sweepStakesManager);
-            marketingFirm.CreateSweepStake();
-
-
-
-            
-            SweepStakes current = marketingFirm.manager.GetSweepStakes();
-
-            while (true)
-            {
-                Contestant contestant = new Contestant();
-                string answer = UserInterface.GetUserInputFor("Type Enter to enter contestant info for the sweepstakes. Type end to end entry submissions.");
-                
-
-                if (answer == "enter")
-                {
-                   current.RegisterContestant(contestant);
-                    Console.Clear();
-                }
-                else if (answer == "end")
-                {
-                    Console.Clear();
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("invalid entry, try again");
-                }
-
-
-
-            }
-            Console.WriteLine("Press enter to pick winner");
-            Contestant winner = current.pickWinner();
-            current.PrintContestantInfo(winner);
-            Console.ReadLine();
-            
-            
+         
         }
     }
 }
